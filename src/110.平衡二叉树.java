55b0c7dcd2a1a16c3d9eb6b96b46d233d7d84d7b
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-21 19:18:14 +0800

    [110] 平衡二叉树

---
+++ src/110.平衡二叉树.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = height(node.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = height(node.right);
+        if (right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+
+    }
+}
+// @lc code=end
+
