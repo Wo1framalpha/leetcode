1db0e5edc4ff20f4666dfbb6a536291ba36f94d3
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-21 19:18:22 +0800

    [102] 二叉树的层序遍历

---
+++ src/102.二叉树的层序遍历.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ret = new ArrayList<>();
+        Deque<TreeNode> q = new LinkedList<>();
+        q.addLast(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> layer = new ArrayList<>();
+            
+            // 使用n和for循环将一层的结果全部遍历
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.pollFirst();
+                layer.add(node.val);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+
+            ret.add(layer);
+        }
+        return ret;
+    }
+}
+// @lc code=end
+
