7798dd9c16901dd0d26e3ee59a4c65129d961d62
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-22 13:45:27 +0800

    [297] 二叉树的序列化与反序列化 DFS

---
+++ src/297.二叉树的序列化与反序列化 前序遍历.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=297 lang=java
+ *
+ * [297] 二叉树的序列化与反序列化
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "X";
+        }
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new  LinkedList<>(Arrays.asList(data.split(",")));
+
+        return parse(queue);
+    }
+
+    public TreeNode parse(Queue<String> queue) {
+        String value = queue.poll();
+        if ("X".equals(value)) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.valueOf(value));
+
+        node.left = parse(queue);
+        node.right = parse(queue);
+
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+// @lc code=end
