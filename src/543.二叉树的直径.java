832b8b27725d4360af6fd5b00449330cb1ff8d71
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-22 14:17:46 +0800

    [543] 二叉树的直径

---
+++ src/543.二叉树的直径.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=java
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+// @lc code=end
+
