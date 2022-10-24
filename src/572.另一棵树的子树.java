75016a44ffe49d1564b8dd65d31da9c3c2842c45
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-24 17:40:51 +0800

    [572] 另一棵树的子树

---
+++ src/572.另一棵树的子树.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=java
+ *
+ * [572] 另一棵树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+            if (isSameTree(root, subRoot)) {
+                return true;
+            } else {
+                if (root.left != null && isSubtree(root.left, subRoot)) {
+                    return true;
+                }
+                if (root.right != null && isSubtree(root.right, subRoot)) {
+                    return true;
+                }
+                return false;
+            }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+// @lc code=end
