56b013cbf89fb76ce45f6319c69680ccba38f1b1
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-10-24 17:40:42 +0800

    [100] 相同的树

--- src/100.相同的树.java
+++ src/100.相同的树.java
@@ -8,44 +8,28 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+        if (p == null && q == null)
             return true;
-        }
-        try {
-            dfs(p,q);
-            return true;
-        } catch (NullPointerException e) {
-            // TODO: handle exception
+        if (p == null || q == null)
             return false;
-        }
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
-    }
-
-    public void dfs(TreeNode p, TreeNode q) {
-        if (p.val != q.val) {
-            throw new NullPointerException();
-        }
-        if (p.left != null || q.left != null) {
-            dfs(p.left, q.left);
-        }
-        if (p.right != null || q.right != null) {
-            dfs(p.right, q.right);
-        }
     }
 }
 // @lc code=end
-
