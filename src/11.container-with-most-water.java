2a6b05bd09a4316a1d2ab7ab87bbc506158c5038
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-11-01 19:25:58 +0800

    [11] Container With Most Water

--- src/11.container-with-most-water.java
+++ src/11.container-with-most-water.java
@@ -7,8 +7,19 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-        
+        int res = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while (i<j) {
+            int tmp = Math.min(height[i],height[j])*(j-i);
+            res = Math.max(res, tmp);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
-
