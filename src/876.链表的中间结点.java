70d968c91975f701a118d2473f2951225ad92d7a
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-11-01 19:36:36 +0800

    [876] 链表的中间结点

---
+++ src/876.链表的中间结点.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=java
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode mid = head;
+        ListNode end = head;
+        while (true) {
+            mid = mid.next;
+            end = end.next;
+            if (end.next == null) {
+                break;
+            }
+            end = end.next;
+            if (end.next == null) {
+                break;
+            }
+        }
+
+        return mid;
+    }
+}
+// @lc code=end
+
