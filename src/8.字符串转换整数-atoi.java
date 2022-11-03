949c4cd164062069429f28dcb192d353feb68d1f
Bob <37956663+Wo1framalpha@users.noreply.github.com>
2022-11-03 21:18:49 +0800

    [8] 字符串转换整数 (atoi)

---
+++ src/8.字符串转换整数-atoi.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode.cn id=8 lang=java
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String s) {
+        int sign = 1, ans = 0, index = 0;
+        char[] array = s.toCharArray();
+        while (index < array.length && array[index] == ' ') {
+            index ++;
+        }
+        if (index < array.length && (array[index] == '-' || array[index] == '+')) {
+            sign = array[index++] == '-' ? -1 : 1;
+        }
+        while (index < array.length && array[index] <= '9' && array[index] >= '0') {
+            int digit = array[index++] - '0';
+            if (ans > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            ans = ans * 10 + digit;
+        }
+        return ans * sign;
+    }
+}
+// @lc code=end
